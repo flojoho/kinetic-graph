e5e14bac1d9cd11f2b1caeecded51c9c551b2165
flojoho <hofkas.flo@gmx.at>
2023-10-08 17:49:14 +0200

    added button highlighting

--- modules/ui/Toolbar.js
+++ modules/ui/Toolbar.js
@@ -6,7 +6,11 @@ Toolbar.mode = 'edge';
 export default Toolbar;
 nodeButton.addEventListener('click', e => {
     Toolbar.mode = 'node';
+    nodeButton.classList.add('button-active');
+    edgeButton.classList.remove('button-active');
 });
 edgeButton.addEventListener('click', e => {
     Toolbar.mode = 'edge';
+    nodeButton.classList.remove('button-active');
+    edgeButton.classList.add('button-active');
 });
--- modules/ui/Toolbar.ts
+++ modules/ui/Toolbar.ts
@@ -10,7 +10,13 @@ export default class Toolbar {
 
 nodeButton.addEventListener('click', e => {
   Toolbar.mode = 'node';
+
+  nodeButton.classList.add('button-active');
+  edgeButton.classList.remove('button-active');
 });
 edgeButton.addEventListener('click', e => {
   Toolbar.mode = 'edge';
+
+  nodeButton.classList.remove('button-active');
+  edgeButton.classList.add('button-active');
 });
